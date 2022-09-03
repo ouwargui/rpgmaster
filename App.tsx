d9347441207f7f6584da2a16e2afe4c2e0861503
ouwargui <guikemon2@gmail.com>
2022-09-03 20:56:39 -0300

    feat: add tailwindcss because styled components isnt working rn

--- App.tsx
+++ App.tsx
@@ -2,18 +2,15 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {StatusBar} from 'expo-status-bar';
-import {ThemeProvider} from 'styled-components/native';
 import StackRouter from './src/routes/Stack';
 import AuthProvider from './src/contexts/AuthProvider';
-import theme from './src/theme/default';
+import defaultTheme from './src/theme/default';
 
 const App: React.FC = () => (
   <AuthProvider>
     <NavigationContainer>
-      <ThemeProvider theme={theme}>
-        <StatusBar style="auto" />
-        <StackRouter />
-      </ThemeProvider>
+      <StatusBar style="light" />
+      <StackRouter />
     </NavigationContainer>
   </AuthProvider>
 );
--- src/theme/default.ts
+++ src/theme/default.ts
@@ -2,4 +2,12 @@ export default {
   colors: {
     background: '#00141A' as const,
   },
+  font: {
+    colors: {
+      text: '#fff' as const,
+    },
+    family: {
+      regular: 'Arial' as const,
+    },
+  },
 };
