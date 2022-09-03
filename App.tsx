7fe0db4998361dbc881aa5334af360fbe6f8a291
ouwargui <guikemon2@gmail.com>
2022-09-03 14:42:13 -0300

    chore: add types to styled-components

--- App.tsx
+++ App.tsx
@@ -2,14 +2,18 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {StatusBar} from 'expo-status-bar';
+import {ThemeProvider} from 'styled-components/native';
 import StackRouter from './src/routes/Stack';
 import AuthProvider from './src/contexts/AuthProvider';
+import theme from './src/theme/default';
 
 const App: React.FC = () => (
   <AuthProvider>
     <NavigationContainer>
-      <StatusBar style="auto" />
-      <StackRouter />
+      <ThemeProvider theme={theme}>
+        <StatusBar style="auto" />
+        <StackRouter />
+      </ThemeProvider>
     </NavigationContainer>
   </AuthProvider>
 );
---
+++ src/theme/default.ts
@@ -0,0 +1,5 @@
+export default {
+  colors: {
+    background: '#00141A' as const,
+  },
+};
