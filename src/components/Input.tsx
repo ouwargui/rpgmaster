e496c07ee4b21ad11365f218a7ce667ffb70be2b
ouwargui <guikemon2@gmail.com>
2022-09-03 23:12:41 -0300

    feat: add input component

---
+++ src/components/Input.tsx
@@ -0,0 +1,67 @@
+import React, {useState} from 'react';
+import {TextInput, TouchableOpacity, View} from 'react-native';
+import Feather from '@expo/vector-icons/Feather';
+
+interface InputProps {
+  contentType?: 'emailAddress' | 'password';
+  placeholder?: string;
+  value: string;
+  onChangeText: (text: string) => void;
+}
+
+const Input: React.FC<InputProps> = ({
+  contentType,
+  placeholder,
+  value,
+  onChangeText,
+  ...props
+}) => {
+  const [viewPassword, setViewPassword] = useState(false);
+
+  return (
+    <View
+      className="
+        p-2
+        w-full
+        h-14
+        bg-[#091B24]
+        border-zinc-800
+        border-[1px]
+        rounded-md
+        justify-start
+        items-center
+        flex-row
+      "
+      {...props}
+    >
+      <TextInput
+        className={`text-zinc-300 ${
+          contentType === 'password' ? 'w-11/12' : 'w-full'
+        }`}
+        keyboardAppearance="dark"
+        placeholderTextColor="rgb(161, 161, 170)"
+        textContentType={contentType}
+        placeholder={placeholder}
+        value={value}
+        onChangeText={onChangeText}
+        selectTextOnFocus
+        secureTextEntry={contentType === 'password' && !viewPassword}
+      />
+      {contentType === 'password' && (
+        <TouchableOpacity
+          activeOpacity={0.6}
+          className="flex-1 justify-center items-center"
+          onPress={() => setViewPassword(!viewPassword)}
+        >
+          {!viewPassword ? (
+            <Feather name="eye" size={24} color="rgb(161, 161, 170)" />
+          ) : (
+            <Feather name="eye-off" size={24} color="rgb(161, 161, 170)" />
+          )}
+        </TouchableOpacity>
+      )}
+    </View>
+  );
+};
+
+export default Input;
