0a823a5a839bb515cc3763aed5f3cdcc1893c2f9
ouwargui <guikemon2@gmail.com>
2022-09-14 02:36:13 -0300

    feat: add react hook form to deal with signup form

--- src/components/SignupForm.tsx
+++ src/components/SignupForm.tsx
@@ -1,7 +1,10 @@
 import React, {useState} from 'react';
 import {Platform, Text, TouchableOpacity, View} from 'react-native';
 import Feather from '@expo/vector-icons/Feather';
-import Input from './Input';
+import {useForm} from 'react-hook-form';
+import * as yup from 'yup';
+import {yupResolver} from '@hookform/resolvers/yup';
+import ControlledInput from './ControlledInput';
 import Button from './Button';
 import FloatingActionButton from './FloatingActionButton/FloatingActionButton';
 import LoginWithGoogle from './Auth/LoginWithGoogle';
@@ -16,17 +19,41 @@ interface SignupFormProps {
   ) => Promise<void>;
 }
 
+interface SignupFormData {
+  name: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+}
+
+const scheme = yup.object({
+  name: yup.string().required('Nome é obrigatório'),
+  email: yup.string().email('E-mail inválido').required('E-mail é obrigatório'),
+  password: yup
+    .string()
+    .min(6, 'Senha deve ter no mínimo 6 caracteres')
+    .required('Senha é obrigatório'),
+  confirmPassword: yup
+    .string()
+    .min(6, 'Senha deve ter no mínimo 6 caracteres')
+    .oneOf([yup.ref('password')], 'Senhas não conferem'),
+});
+
 const SignupForm: React.FC<SignupFormProps> = ({
   handleAnimationState,
   handleSignup,
 }) => {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
+  const {
+    control,
+    handleSubmit,
+    formState: {errors},
+  } = useForm<SignupFormData>({
+    resolver: yupResolver(scheme),
+  });
+
   const [isLoading, setIsLoading] = useState(false);
 
-  const handlePressSignup = async () => {
+  const handlePressSignup = async ({name, email, password}: SignupFormData) => {
     setIsLoading(true);
     await handleSignup(name, email, password);
     setIsLoading(false);
@@ -48,41 +75,46 @@ const SignupForm: React.FC<SignupFormProps> = ({
       </View>
       <View className="flex-1 w-full">
         <View className="w-full mb-4">
-          <Input placeholder="Nome" value={name} onChangeText={setName} />
+          <ControlledInput
+            control={control}
+            name="name"
+            placeholder="Nome"
+            errors={errors.name}
+          />
         </View>
         <View className="w-full mb-4">
-          <Input
+          <ControlledInput
+            control={control}
+            name="email"
             placeholder="E-mail"
-            value={email}
-            onChangeText={setEmail}
             contentType="emailAddress"
+            errors={errors.email}
           />
         </View>
         <View className="w-full mb-4">
-          <Input
+          <ControlledInput
+            control={control}
+            name="password"
             placeholder="Senha"
-            value={password}
-            onChangeText={setPassword}
             contentType="password"
+            errors={errors.password}
           />
         </View>
         <View className="w-full mb-4">
-          <Input
+          <ControlledInput
+            control={control}
+            name="confirmPassword"
             placeholder="Confirme a sua senha"
-            value={confirmPassword}
-            onChangeText={setConfirmPassword}
             contentType="password"
+            errors={errors.confirmPassword}
           />
         </View>
       </View>
       <View className="w-full justify-evenly items-center flex-row">
         <Button
           title="Continuar"
-          disabled={
-            !name || !email || !password || password !== confirmPassword
-          }
           isLoading={isLoading}
-          onPress={handlePressSignup}
+          onPress={handleSubmit(handlePressSignup)}
         />
         <View>
           <FloatingActionButton>
